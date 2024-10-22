91aace64bd2113e28fa919fed0af025ecfc10ed2
Andre <acardoso.ny@gmail.com>
2024-10-22 09:15:29 +0100

    testing file and acttion

--- testing.py
+++ testing.py
@@ -1,5 +1,12 @@
-from WordleTwitterBot.pyautogui_SolveMain import Solve
+import os
+import tweepy
+from datetime import datetime, timedelta
 
+twitterAPIaccess = tweepy.Client(
+                # bearer_token=os.environ.get('TWITTER_BEARER_TOKEN'),
+                consumer_key=os.environ.get('TWITTER_API_KEY'), consumer_secret=os.environ.get('TWITTER_API_KEY_SECRET'),
+                access_token=os.environ.get('TWITTER_ACCESS_TOKEN'), access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+                )
 
-solving = Solve()
-print(solving.ComputerGuesses())
+temp = twitterAPIaccess.search_all_tweets("hello world")
+print(temp)
